bf3a2ed52831762510c42d4813cc0a3bdece139e
Денис Cтоляров <denis2002.stolyarov@mail.ru>
2022-03-17 23:18:30 +0700

    Добавьте файлы проекта.

---
+++ RegexLaba/Models/FileController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexLaba.Models
+{
+    public class FileConroller
+    {
+        public void Write(string data, string path)
+        {
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.Write(data);
+            }
+        }
+        public string Read(string path)
+        {
+            string output = "";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                return sr.ReadToEnd();
+
+            }
+        }
+    }
+}
---
+++ RegexLaba/Models/RegExFinder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexLaba.Models
+{
+    public class RegExFinder
+    {
+        public string[] GetMatches(string pattern, string input)
+        {
+            Regex rg = new Regex(pattern);
+            return rg.Matches(input)
+                .Cast<Match>()
+                .Select(m => m.Value)
+                .ToArray();
+
+        }
+    }
+}
